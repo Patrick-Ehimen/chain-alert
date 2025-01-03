e3e493a54926245a9842d316b511f36ccb53a685
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2025-01-03 22:25:40 +0100

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -42,5 +42,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log('Telegram bot is active and listening for commands');
+  console.log("Telegram bot is active and listening for commands");
 });
